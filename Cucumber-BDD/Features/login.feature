163c4c8fe8acea6836e061942a5c1e9c00b44a9b
SaranHanru <saran.27.hanru@gmail.com>
2022-03-15 19:55:37 +0530

    New Commit Of BDD

---
+++ Cucumber-BDD/Features/login.feature
@@ -0,0 +1,11 @@
+Feature: Login
+
+Scenario: Successfull Login using Credentials
+
+Given User Launch Chrome Browser
+
+When User opens a URL "http://localhost:8888/"
+
+And User enters Email as "admin" and password as "admin"
+
+And Click on Login Button
---
+++ Cucumber-BDD/src/test/java/stepDefinition/Steps.java
@@ -0,0 +1,35 @@
+package stepDefinition;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.*;
+
+public class Steps {
+	
+	public WebDriver driver;
+	
+	@Given("User Launch Chrome Browser")
+	public void user_launch_chrome_browser() {
+		driver=new ChromeDriver();
+	}
+
+	@When("User opens a URL {string}")
+	public void user_opens_a_url(String url) {
+	    driver.get(url);
+	}
+
+	@When("User enter33s Email as {string} and password as {string}")
+	public void user_enters_email_as_and_password_as(String username, String password) {
+	   driver.findElement(By.name("user_name")).sendKeys(username);
+	   driver.findElement(By.name("user_password")).sendKeys(password);
+	}
+
+	@When("Click on Login Button")
+	public void click_on_login_button() {
+	    driver.findElement(By.id("submitButton")).click();
+	}
+
+
+}
